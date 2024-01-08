6fffd30eb7c50224db0bc373077f2f27609bbf3c
LucasMagaldi <lucas.magaldi@hotmail.com>
2024-01-08 00:04:22 -0300

    feat : implement router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from "styled-components"
+import { BrowserRouter } from "react-router-dom"
 
 import { GlobalStyle } from "./styles/Global"
 import { defaultTheme } from "./styles/themes/default"
+import { Router } from "./Router"
 
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
         App
       <GlobalStyle/>
     </ThemeProvider>
